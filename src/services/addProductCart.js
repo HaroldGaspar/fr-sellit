3573ea41ab59c949c60542ac0607216f75cd46b2
HaroldGaspar <gaspar.hrl@gmail.com>
2021-10-04 15:03:18 -0500

    addProductCart cors error fixed

--- src/services/addProductCart.js
+++ src/services/addProductCart.js
@@ -3,19 +3,32 @@ import React from "react";
 export async function addProductCart(product) {
   const cart = localStorage.getItem("cart");
   const token = localStorage.getItem("token");
-  const resCartProduct = await fetch(
-    `http://hakhi.xyz/:8000/product-details/`,
-    {
-      method: "post",
-      headers: new Headers({
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      }),
-      body: JSON.stringify({
-        cart,
-        qty: 1,
-        product,
-      }),
-    }
-  );
+
+  //PROBLEMS WITH CORS?
+  // fetch("http://hakhi.xyz/:8000/product-details/", {
+  //   method: "post",
+  //   headers: new Headers({
+  //     "Access-Control-Allow-Origin": "*",
+  //     Authorization: `Bearer ${token}`,
+  //     "Content-Type": "application/json",
+  //   }),
+  //   body: JSON.stringify({
+  //   cart,
+  //   qty: 1,
+  //   product,
+  // }),
+  // });
+
+  fetch("http://hakhi.xyz:8000/product-details", {
+    method: "post",
+    headers: new Headers({
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    }),
+    body: JSON.stringify({
+      cart,
+      qty: 1,
+      product,
+    }),
+  });
 }
--- src/services/getPopularProducts.js
+++ src/services/getPopularProducts.js
@@ -1,6 +1,11 @@
 export async function getPopularProducts(products, setProducts) {
   const res = await fetch(
-    "http://hakhi.xyz:8000/products?_sort=rating:DESC&_limit=3"
+    "http://hakhi.xyz:8000/products?_sort=rating:DESC&_limit=3",
+    {
+      headers: new Headers({
+        "Access-Control-Allow-Origin": "*",
+      }),
+    }
   );
   const data = await res.json();
   setProducts(data);
