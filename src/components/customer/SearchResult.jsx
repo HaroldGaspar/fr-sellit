50e8e3a7f26f1ecb2dd531f55abe14b050bb2a66
HaroldGaspar <gaspar.hrl@gmail.com>
2021-10-08 00:23:18 -0500

    search fx & view added

---
+++ src/components/customer/SearchResult.jsx
@@ -0,0 +1,40 @@
+import React, { useCallback, useEffect, useRef, useState } from "react"
+import { GifsList, Spinner } from "components"
+import { useGifs, useNearScreen } from "hooks"
+import debounce from "just-debounce-it"
+import { searchProduct } from "services"
+import { useHistory, useParams } from "react-router"
+import ProductHome from "./ProductHome"
+import { Nav } from "components/Nav"
+
+export function SearchResults(props) {
+  console.log("props ", props)
+  const { keyword } = useParams()
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    searchProduct(keyword, setProducts)
+  }, [])
+  return (
+    <>
+      <Nav />
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <h3>
+            Search <b>{keyword}</b>
+          </h3>
+          {/* <GifsList gifs={gifs} /> */}
+          {products.map((product) => (
+            <ProductHome product={product} />
+          ))}
+        </>
+      )}
+      <br />
+      {/* <button onClick={handleNextPage}>Get next page</button> */}
+    </>
+  )
+}
---
+++ src/services/customer/searchProduct.js
@@ -0,0 +1,11 @@
+import { API_URL } from "../settings"
+
+export async function searchProduct(keyword, setProducts) {
+  //   setloading(true)
+  const resProducts = await fetch(`${API_URL}/products?_q=${keyword}`)
+
+  const resD = await resProducts.json()
+  console.log("search ", resD)
+  setProducts(resD)
+  // setloading(false)
+}
