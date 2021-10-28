0d3388a168b61838bcdec1f9e1a293d1becbc417
HaroldGaspar <gaspar.hrl@gmail.com>
2021-10-27 20:43:45 -0500

    productDetail avance

--- src/components/public/find/ProductDetail.jsx
+++ src/components/public/find/ProductDetail.jsx
@@ -1,6 +1,9 @@
 import { ReviewForm } from "../../customer/review/ReviewForm"
 import { ReviewsList } from "../../customer/review/ReviewsList"
 import styled from "styled-components"
+import { useEffect, useState } from "react"
+import { getProduct } from "services"
+import { useParams } from "react-router"
 
 const ContainerHome = styled.div`
   min-height: 100vh;
@@ -10,20 +13,31 @@ const ContainerHome = styled.div`
   padding: 0 11px;
 `
 export function ProductDetail() {
+  const { id } = useParams()
+  const [productDetail, setProductDetail] = useState()
+  const [loading, setloading] = useState()
+  useEffect(() => {
+    getProduct(id, setProductDetail, setloading)
+  }, [])
+
   return (
     <>
       <ContainerHome>
+        {/* {loading ? ( */}
         <div className="container">
           <div className="productdetail">
             <h2 className="productdetail__title">Detail of product</h2>
             <div className="productdetail__img">product photo</div>
-            <div className="productdetail__name">name</div>
+            <div className="productdetail__name">{productDetail?.name}</div>
           </div>
           <div className="productreview">
             <ReviewsList />
             <ReviewForm />
           </div>
         </div>
+        {/* ) : (
+          ""
+        )} */}
       </ContainerHome>
     </>
   )
